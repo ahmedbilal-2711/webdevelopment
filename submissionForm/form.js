4e3d0fc64c483ad04b5a678b602ba30f2a77dc69
Ahmed Bilal <ahmedbilal6880@gmail.com>
2021-09-05 16:54:00 +0500

    menu + about us added

--- submissionForm/form.js
+++ submissionForm/form.js
@@ -1,12 +1,12 @@
-console.log("Hello");
-let age = prompt("What is your age?",18);
-if(age<18){
-    alert("You are not allowed to fill the form")
-}
-else{
-    confirm("Are you sure for the Dubai trip")
-}
+// console.log("Hello");
+// let age = prompt("What is your age?",18);
+// if(age<18){
+//     alert("You are not allowed to fill the form")
+// }
+// else{
+//     confirm("Are you sure for the Dubai trip")
+// }
 
-let footCenter = document.getElementById('center');
-footCenter.innerHTML = "No copy rights";
-console.log(footCenter);
+// let footCenter = document.getElementById('center');
+// footCenter.innerHTML = "No copy rights";
+// console.log(footCenter);
