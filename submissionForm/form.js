3dfc8ad77328471c5fe66c81f3619afaeffcf5b1
Ahmed Bilal <ahmedbilal6880@gmail.com>
2021-09-08 10:55:29 +0500

    tried a clock in js

--- submissionForm/form.js
+++ submissionForm/form.js
@@ -10,3 +10,15 @@
 // let footCenter = document.getElementById('center');
 // footCenter.innerHTML = "No copy rights";
 // console.log(footCenter);
+
+let dt;
+setInterval(() => {
+    dt = new Date();
+    let hr = dt.getHours();
+    let min = dt.getMinutes();
+    let sec = dt.getSeconds();
+    document.getElementById('time').innerHTML = hr+":"+min+":"+sec;
+}, 1000);
+
+
+let test=(a,b) => console.log("The sum is: "+(a+b));
