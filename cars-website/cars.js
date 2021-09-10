8dedc4b2dfeaeb6af9dfda734c28696bc2073aab
Ahmed Bilal <ahmedbilal6880@gmail.com>
2021-09-10 17:19:59 +0500

    top+nav done

--- cars-website/cars.js
+++ cars-website/cars.js
@@ -1,73 +1,81 @@
 // Changing images automatically
 let slideNum = 0;
+let specsNum = 0;
 function slide() {
   var getImg = document.getElementsByClassName("carSlide");
   for (let i = 0; i < getImg.length; i++) {
-    getImg[i].style.display = "none";
+      getImg[i].style.display = "none";
   }
   slideNum++;
   if (slideNum > getImg.length) {
-    slideNum = 1;
-  }
-  getImg[slideNum - 1].style.display = "block";
-  imgInd(slideNum - 1);
-
-  if (slideNum=0) {
-    document.getElementById("carNameSpecs").innerHTML = "Audi";
-    console.log("Audi");
-}
-else{
-    document.getElementById("carNameSpecs").innerHTML = "xyz";
-    console.log("xyz");
-}
-  
+      slideNum = 1;
+    }
+    getImg[slideNum - 1].style.display = "block";
+    imgInd(slideNum - 1);
 }
 setInterval(slide, 4000);
-
-let buy = document.getElementById("BuyNow");
 
 // Circles Indication to Changing Images
 let imgInd = (i) => {
   let j = 0;
+  let hp = document.getElementById("hp");
+  let rim = document.getElementById("rim");
+  let seat = document.getElementById("seat");
+  let tr = document.getElementById("tr");
+  
   for (; j < 6; j++) {
     if (j != i) {
         document.getElementById("c" + j).style.backgroundColor = "black";
     } else {
         document.getElementById("c" + i).style.backgroundColor ="rgb(197, 150, 150)";
+        if (i==0) {
+            let cns=document.getElementById("carNameSpecs");
+            cns.innerHTML="Audi";
+            hp.innerHTML="1000 HP";
+            rim.innerHTML="Alloy";
+            seat.innerHTML="Leather Black";
+            tr.innerHTML="Yes";
+        }
+        else if (i==1) {
+            let cns=document.getElementById("carNameSpecs");
+            cns.innerHTML="Audi";
+            hp.innerHTML="1200 HP";
+            rim.innerHTML="Alloy";
+            seat.innerHTML="Leather Brown";
+            tr.innerHTML="Yes";
+        }
+        else if (i==2) {
+            let cns=document.getElementById("carNameSpecs");
+            cns.innerHTML="Sports Car";
+            hp.innerHTML="1800 HP";
+            rim.innerHTML="Alloy";
+            seat.innerHTML="Leather Yellow";
+            tr.innerHTML="No";
+        }
+        else if (i==3) {
+            let cns=document.getElementById("carNameSpecs");
+            cns.innerHTML="Chevorlet";
+            hp.innerHTML="1200 HP";
+            rim.innerHTML="Alloy";
+            seat.innerHTML="No Covers";
+            tr.innerHTML="No";
+        }
+        else if (i==4) {
+            let cns=document.getElementById("carNameSpecs");
+            cns.innerHTML="Mercedes";
+            hp.innerHTML="1600 HP";
+            rim.innerHTML="Alloy";
+            seat.innerHTML="Leather Black";
+            tr.innerHTML="Yes";
+        }
+        else {
+            let cns=document.getElementById("carNameSpecs");
+            cns.innerHTML="Range Rover";
+            hp.innerHTML="2000 HP";
+            rim.innerHTML="Alloy";
+            seat.innerHTML="Leather Red";
+            tr.innerHTML="Yes";
+        }
     }
   }
 };
-
-//Display Specs
-function specs(s) {
-    if (s=0) {
-        document.getElementById("carNameSpecs").innerHTML = "Audi";
-        console.log("Audi");
-        console.log(s);
-    }
-    else if(s=1){
-        document.getElementById("carNameSpecs").innerHTML = "xyz";
-        console.log("Audi");
-        console.log(s);
-    }
-    else if(s=2){
-        document.getElementById("carNameSpecs").innerHTML = "Sports Car";
-        console.log("Sports Car");
-        console.log(s);
-    }
-    else if(s=3){
-        document.getElementById("carNameSpecs").innerHTML = "Chavorlet";
-        console.log("Chavorlet");
-        console.log(s);
-    }
-    else if(s=4){
-        document.getElementById("carNameSpecs").innerHTML = "Mercedes";
-        console.log("Mercedes");
-        console.log(s);
-    }
-    else{
-        document.getElementById("carNameSpecs").innerHTML = "Range Rover";
-        console.log("Range Rover");
-        console.log(s);
-    }
-}
